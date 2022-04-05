d845ffb6dd568151b15743822506b62e50de9dd6
Cody Snell <codyjsnell@yahoo.com>
2022-04-05 09:01:42 -0500

    implicit returns

--- src/components/card-list/CardList.component.js
+++ src/components/card-list/CardList.component.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { Card } from '../card/Card.component'
 import './CardList.styles.css'
 
-const CardList = ({ monsters }) => {
-  return (
-    <div className="card-list">
-      {monsters.map((monster) => (
-        <Card key={monster.id} monster={monster} />
-      ))}
-    </div>
-  )
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+)
 
 export default CardList
